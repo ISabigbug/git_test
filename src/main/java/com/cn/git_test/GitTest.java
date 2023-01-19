0936b4e32d16ff31c88bdc9561939007e111e81f
Chasel <2081915208@qq.com>
2023-01-19 19:44:22 +0800

    master commit2

--- src/main/java/com/cn/git_test/GitTest.java
+++ src/main/java/com/cn/git_test/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("Hello,Git!");
         System.out.println("Hello,Git!");
         System.out.println("Hello,Git!");
+        System.out.println("test master");
     }
 }
