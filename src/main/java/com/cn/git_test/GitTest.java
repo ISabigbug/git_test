65b17a9a55829fa2d967c139db97d2b51d5b68f0
Chasel <2081915208@qq.com>
2023-01-19 19:39:02 +0800

    hot-fix commit1

--- src/main/java/com/cn/git_test/GitTest.java
+++ src/main/java/com/cn/git_test/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("Hello,Git!");
         System.out.println("Hello,Git!");
         System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!");
     }
 }
