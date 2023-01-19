8e8122c9dda7083b8811cb1f36ef9024f8ffdf7e
Chasel <2081915208@qq.com>
2023-01-19 17:23:38 +0800

    idea first commit

---
+++ src/main/java/com/cn/git_test/GitTest.java
@@ -0,0 +1,15 @@
+package com.cn.git_test;
+
+/**
+ * @BelongsProject: git_test
+ * @BelongsPackage: com.cn.git_test
+ * @Author:
+ * @CreateTime: 2023-01-19  16:52
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+    }
+}
